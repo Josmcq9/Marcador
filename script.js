d0d2876586de531e00b820d80a3d922152be07d6
Josmcq9 <109076731+Josmcq9@users.noreply.github.com>
2023-11-28 19:21:45 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function actualizarMarcador() {
   document.getElementById("puntuacion1").textContent = puntuacion1;
   document.getElementById("puntuacion2").textContent = puntuacion2;
 }
+function toggleFullScreen() {
+      const elem = document.documentElement;
+
+      if (
+        !document.fullscreenElement &&
+        !document.mozFullScreenElement &&
+        !document.webkitFullscreenElement &&
+        !document.msFullscreenElement
+      ) {
+        // Entrar en modo de pantalla completa
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) {
+          elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) {
+          elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+          elem.msRequestFullscreen();
+        }
+      } else {
+        // Salir del modo de pantalla completa
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      }
+    }
